04a340b62c7b5c2e035c61c75f436723dc9fe0c5
Arooon4 <aprasadsubramaniyan@gmail.com>
2026-09-11 19:45:38 +0530

    Fix ESLint build errors for Vercel

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,7 +9,11 @@ function NavBar() {
                Home | </li>
             <li>Products | </li>
             <li><a href="#">About </a>| </li>
+<<<<<<< HEAD
             <li><a href="#">Contact </a> </li>
+=======
+            <li><a href="#contact">Contact </a> </li>
+>>>>>>> b6963d9 (Fix ESLint build errors for Vercel)
         </ul>
       </nav>
     );
